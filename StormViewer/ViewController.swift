3de108c9e421f1b5b97c0fe3289053a614e07d53
Marina Khort <maryna.khort@gmail.com>
2020-09-01 00:49:58 +0300

    GCD added

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -17,18 +17,26 @@ class ViewController: UITableViewController {
 		title = "Storm Viewer"
 		
 		navigationController?.navigationBar.prefersLargeTitles = true
+		
+		performSelector(inBackground: #selector(loadingImages), with: nil)
+	}
+	
+	@objc func loadingImages() {
 		let fm = FileManager.default
 		let path = Bundle.main.resourcePath!
 		let items = try! fm.contentsOfDirectory(atPath: path)
-		
+
 		for item in items {
 			if item.hasPrefix("nssl") {
-				pictures.append(item)
+				self.pictures.append(item)
 			}
 		}
-		print(pictures)
+		self.pictures.sort(by: <)
+		
+		DispatchQueue.main.async {
+			self.tableView.reloadData()
+		}
 	}
-	
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return pictures.count
@@ -37,7 +45,6 @@ class ViewController: UITableViewController {
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-		pictures.sort(by: <)
 		cell.textLabel?.text = pictures[indexPath.row]
 		return cell
 	}
